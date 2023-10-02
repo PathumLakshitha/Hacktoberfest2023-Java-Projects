c42c5e628f1c3d944518d5cbb9eba3c53387c0df
Pathum Lakshitha <96337912+PathumLakshitha@users.noreply.github.com>
2023-10-03 00:40:33 +0530

    Create ArrayList.java

---
+++ pathum-lakshitha/ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Tesla");
+    cars.add("BMW");
+    cars.add("TOYOTA");
+    cars.add("Hyundai");
+    cars.add("Honda");
+    System.out.println(cars);
+  } 
+}
